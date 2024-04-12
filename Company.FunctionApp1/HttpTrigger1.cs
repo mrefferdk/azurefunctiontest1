674fe99af210c9fddfadb8f6152c7a47256ec366
Thomas Effersøe <thoeff@clio.me>
2024-04-12 13:14:18 +0200

    Testing

--- Company.FunctionApp1/HttpTrigger1.cs
+++ Company.FunctionApp1/HttpTrigger1.cs
@@ -19,7 +19,8 @@ public static class HttpTrigger1
     {
         log.LogInformation("C# HTTP trigger function processed a request.");
 
-        string name = req.Query["name"];
+        return (ActionResult)new OkObjectResult($"Hello, UNKNOWN");
+        /*string name = req.Query["name"];
 
         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
         dynamic data = JsonConvert.DeserializeObject(requestBody);
@@ -27,7 +28,7 @@ public static class HttpTrigger1
 
         return name != null
             ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        
+            : new BadRequestObjectResult("Please pass a name on the query string or in the request body");*/
+
     }
 }
